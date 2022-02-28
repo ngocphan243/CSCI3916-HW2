f23bb31d4a258c6822f5514422a5f56ce70f2561
ngocphan243 <98306356+ngocphan243@users.noreply.github.com>
2022-02-27 21:27:27 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,20 +1,18 @@
-// Load required packages
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
 
 passport.use(new BasicStrategy(
-    function(username, password, done) {
-        //hard coded
-        var user = { name: "testuser" };
-        if (username == user.name && password == "cu")
-        {
-            return done(null, user);
-        }
-        else
-        {
-            return done(null, false);
-        }
-    }
+   function(username, password, done) {
+       var user = { name: "cu_user"}; //could have called to a database to look this up
+       if (username === user.name && password === "cu_rulez") // tripple equal is type and value; double == is just equal
+       {
+           return done(null, user);
+       }
+       else
+       {
+           return done(null, false);
+       }
+   }
 ));
 
-exports.isAuthenticated = passport.authenticate('basic', { session : false });
+exports.isAuthenticated = passport.authenticate('basic', { session: false });
